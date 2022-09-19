eae9e80d51a5b694a43d60221b90e595ad25edb7
CodingC1402 <cornycodingcorn@gmail.com>
2022-09-19 13:16:09 +0700

    Lighter src and build with configs

--- build.sh
+++ build.sh
@@ -1,1 +1,33 @@
-mkarchiso -v -w ./.build -o ./.release ./src
+dir=$(dirname -- "$0")
+echo "building from $dir"
+
+workDir="${dir}/.build"
+outDir="${dir}/.release"
+profileDir="${dir}/src"
+
+echo "Work: ${workDir}"
+echo "Out: ${outDir}"
+echo "Profile: ${profileDir}"
+
+echo "options:"
+echo "1) update build"
+echo "2) clean build"
+echo "3) clear and clean build"
+
+read opt
+
+case $opt in
+	1)
+		;;
+	2)
+		rm -r $workDir
+		;;
+	3)
+		rm -r $workDir
+		rm -r $outDir
+		;;
+	*)
+		;;
+esac
+		
+mkarchiso -v -w $workDir -o $outDir $profileDir
